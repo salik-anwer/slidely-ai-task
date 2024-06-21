c2e4671e358c7d9f08fe7f911ab137e45ff02849
salik-anwer <ansarisalik99@gmail.com>
2024-06-21 17:28:41 +0530

    added endpoints for submission functionality

--- src/form.ts
+++ src/form.ts
@@ -1,19 +1,56 @@
 import { Request, Response } from 'express';
 
 interface Form {
+  id: string;
   title: string;
+  description: string;
   questions: string[];
 }
 
+interface Submission {
+  formId: string;
+  submissionId: string;
+  responses: Array<{ question: string, answer: string }>;
+}
+
 const forms: Form[] = [];
+const submissions: Submission[] = [];
 
 export const createForm = (req: Request, res: Response) => {
-  const { title, questions } = req.body;
-  const newForm: Form = { title, questions };
+  const { id, title, description, questions } = req.body;
+  const newForm: Form = {
+    id,
+    title,
+    description,
+    questions
+  };
   forms.push(newForm);
+  console.log(forms);
   res.status(201).json(newForm);
 };
 
 export const getForms = (req: Request, res: Response) => {
   res.status(200).json(forms);
 };
+
+export const submitForm = (req: Request, res: Response) => {
+  const { formId, submissionId, responses } = req.body;
+  const newSubmission: Submission = { formId, submissionId, responses };
+  submissions.push(newSubmission);
+  console.log(submissions);
+  res.status(201).json(newSubmission);
+};
+
+export const getSubmissions = (req: Request, res: Response) => {
+  res.status(200).json(submissions);
+};
+
+export const getFormByID = (req: Request, res: Response) => {
+  const formId = req.params.id;
+    const form = forms.find(f => f.id === formId);
+    if (form) {
+        res.json(form);
+    } else {
+        res.status(404).send('Form not found');
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { createForm, getForms } from './form';
+import { createForm, getFormByID, getForms, getSubmissions, submitForm } from './form';
 
 const app = express();
 const port = 3000;
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 
 app.post('/create', createForm);
 app.get('/forms', getForms);
+app.post('/submit', submitForm);
+app.get('/submissions', getSubmissions);
+app.get('/form/:id', getFormByID);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
